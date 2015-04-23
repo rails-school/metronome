c7af2ddbde0b2486a19e685de43b3fbadec8b7b0
Ben Taitelbaum <ben@coshx.com>
2015-04-23 19:13:36 -0400

    gets metronome clicking

--- metronome.js
+++ metronome.js
@@ -1,20 +1,49 @@
-console.log("This is JavaScript");
+var Metronome = {
+  playing: false,
 
-function bpm() {
-  return parseInt($("#bpm").val());
+  bpm: function() {
+    return parseInt($("#bpm").val());
+  },
+
+  changeSpeed: function(delta) {
+    $("#bpm").val(Metronome.bpm() + delta);
+  },
+
+  increase: function() {
+    Metronome.changeSpeed(5);
+  },
+
+  decrease: function() {
+    Metronome.changeSpeed(-5);
+  },
+
+
+
+  play: function() {
+    if (Metronome.playing) {
+      $("#play").val("Play");
+      Metronome.playing = false;
+    } else {
+      $("#play").val("Pause");
+      Metronome.playing = true;
+      Metronome.click();
+    }
+  },
+
+  click: function() {
+    if (Metronome.playing) {
+      $("#click")[0].play();
+      console.log("Click!");
+      var msPerBeat = 60*1000 / Metronome.bpm();
+      setTimeout(Metronome.click, msPerBeat);
+    }
+  }
 }
-function changeSpeed(delta) {
-  $("#bpm").val(bpm() + delta);
-}
-function increase() {
-  changeSpeed(5);
-}
-function decrease() {
-  changeSpeed(-5);
-}
+
 
 // on document ready
 $(function() {
-  $("#increase").click(increase);
-  $("#decrease").click(decrease);
+  $("#increase").click(Metronome.increase);
+  $("#decrease").click(Metronome.decrease);
+  $("#play").click(Metronome.play);
 });
